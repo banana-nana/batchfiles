171d75a84c4892bbb8e0bd822958e6e30f6c0f65
banana-nana <50461810+banana-nana@users.noreply.github.com>
2022-04-21 11:02:51 -0500

    useless item

---
+++ opennotepad.bat
@@ -0,0 +1,12 @@
+@ECHO OFF 
+:: This batch file details Windows 10, hardware, and networking configuration.
+TITLE Useless things
+ECHO Please wait... Opening Notepad.
+:: Section 1: Windows 10 information
+notepad.exe
+ECHO ============================
+ECHO CREDITS
+ECHO ============================
+ECHO Author : Banana-nana
+ECHO COPYRIGHT 2022 © BANANA-NANA support@banana-corp.tk
+PAUSE
